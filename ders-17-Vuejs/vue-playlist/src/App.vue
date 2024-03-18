806c984af6158ef8da1045c3b9ca3e9ab129d69c
Betül YEL <yellbetul@gmail.com>
2024-03-18 16:19:42 +0300

    Updated

---
+++ ders-17-Vuejs/vue-playlist/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ title }}</h1>
+  <button v-on:click="changeTitle()">Başlığı Değiştir</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "İlk Vue CLI çalışmam",
+    }
+  },
+  methods: {
+    changeTitle: function () {
+      this.title = "Başlığı Değiştirdim.";
+    }
+  }
+}
+</script>
